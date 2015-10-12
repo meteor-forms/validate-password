a91cd10c5d64a55be0f5c011c0db1bdec3c7faa2
Patrick Lewis <Patrickml.pml@gmail.com>
2015-10-12 14:52:15 -0400

    Removed Startup Function

--- validate-password.js
+++ validate-password.js
@@ -1,16 +1,14 @@
-Meteor.startup(function () {
-  ValidateForm.addValidator('data-password', function ($el, instance) {
-    var value = $el.val();
-    var oldPass = $('[data-password-old]').val();
+ValidateForm.addValidator('data-password', function ($el, instance) {
+  var value = $el.val();
+  var oldPass = $('[data-password-old]').val();
 
-    if (value === oldPass) {
-      instance._showSuccess();
-      instance.log("[ValidateForm] password success", instance.el);
-    } else {
-      instance._showError("Passwords do not match.");
-      instance.log("[ValidateForm] password failed", instance.el);
-    }
+  if (value === oldPass) {
+    instance._showSuccess();
+    instance.log("[ValidateForm] password success", instance.el);
+  } else {
+    instance._showError("Passwords do not match.");
+    instance.log("[ValidateForm] password failed", instance.el);
+  }
 
-    instance._validations.push(value === oldPass);
-  });
+  instance._validations.push(value === oldPass);
 });
